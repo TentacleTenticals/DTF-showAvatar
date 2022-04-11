7a9ccda21d7b80c36d106df33ec501abd66bd481
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2022-04-11 10:25:39 +0300

    Update Tampermonkey script (Alter).js
    Фикс проблемы с запуском скрипта

--- Tampermonkey script (Alter).js
+++ Tampermonkey script (Alter).js
@@ -12,7 +12,9 @@
 (function() {
     'use strict';
 
-document.body.onload = function(){
+  window.addEventListener('load', run)
+
+  function run(){
   console.log('Loaded!');
 let mainFilter = new RegExp(`comment__avatar|content-header-author__avatar|subsite-card__avatar|v-header__cover|v-header-avatar|${document.querySelector("div[class='layout__right-column'] div[style^='background-image").className}`),
     commentsRightBarFilter = new RegExp(document.querySelector("div[class='layout__right-column'] div[style^='background-image").className),
